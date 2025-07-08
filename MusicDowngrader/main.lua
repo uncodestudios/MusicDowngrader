17f80e329261c53047df47b4f7628d6ac5bd196f
Jayden <110271631+uncodestudios@users.noreply.github.com>
2025-07-08 12:29:46 -0500

    Add files via upload

---
+++ MusicDowngrader/main.lua
@@ -0,0 +1,41 @@
+-- File: MusicDowngrader.lua
+-- Author: Nyx
+-- Date: 07/08/2025:12:24 PM
+
+local canRun = false
+local audioSource = nil
+local favClip = nil
+
+function Start()
+    EObject:LoadAudio("Sounds/bad_tag_xmas.wav", "onAudioLoaded", "TemplateExtension")
+end
+
+function onAudioLoaded(audioClip)
+    if not audioClip then
+        Debug:LogError("Failed to load audioclip")
+        return
+    end
+
+    local obj = Engine:FindGameObject("MusicSourceF")
+    
+    if not obj then
+       return
+    end
+
+    audioSource = Engine:GetEngineComponent(obj, "AudioSource")
+
+    if not audioSource then
+       return
+    end
+
+    audioSource.Stop()
+    favClip = audioClip
+    audioSource.clip = audioClip
+    audioSource.Play()
+end
+
+function OnUnloaded()
+    canRun = false
+    favClip = nil
+    audioSource = nil
+end
